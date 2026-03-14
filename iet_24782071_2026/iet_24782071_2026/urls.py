32a391fd60bc9698d01bfaa3b0ed7b581023ca2d
sheva0410 <putrawijaya1342@gmail.com>
2026-03-14 08:31:43 +0700

    pengerjaan labsession2 awal

--- iet_24782071_2026/iet_24782071_2026/urls.py
+++ iet_24782071_2026/iet_24782071_2026/urls.py
@@ -1,15 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Halaman Utama")
-
-def welcome(request):
-    return HttpResponse("Selamat Datang")
+from django.urls import path, include
 
 urlpatterns = [
-    path('', home),
-    path('welcome/', welcome),
     path('admin/', admin.site.urls),
+    path('', include('main_app.urls')),
 ]
